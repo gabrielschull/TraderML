873c27082706e859368292d62701686c1fd23829
Gabriel <gschull991@gmail.com>
2024-03-22 20:05:02 -0400

    dynamic/optional params

--- backend/api.py
+++ backend/api.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 from lumibot.brokers import Alpaca
 from lumibot.backtesting import YahooDataBacktesting
 from tradebot import traderML, ALPACA_CREDS, BASE_URL
+from typing import Optional
 
 app = FastAPI()
 
@@ -18,17 +19,17 @@ app.add_middleware(
 )
 
 class strategyParams(BaseModel):
-    symbol: str
-    cash_at_risk: float
-    start_date: datetime
-    end_date: datetime
-    bracket_buy_take_profit_multiplier: float
-    bracket_buy_stop_loss_multiplier: float
-    bracket_sell_take_profit_multiplier: float
-    bracket_sell_stop_loss_multiplier: float
-    position_size: float
-    order_type: str
-    sentiment_time_to_consider: int
+    symbol: Optional[str] = None
+    cash_at_risk: Optional[float] = None
+    start_date: Optional[datetime] = None
+    end_date: Optional[datetime] = None
+    bracket_buy_take_profit_multiplier: Optional[float] = None
+    bracket_buy_stop_loss_multiplier: Optional[float] = None
+    bracket_sell_take_profit_multiplier: Optional[float] = None
+    bracket_sell_stop_loss_multiplier: Optional[float] = None
+    position_size: Optional[float] = None
+    order_type: Optional[str] = None
+    sentiment_time_to_consider: Optional[int] = None
 
 strategy_instance = None
 broker=Alpaca(ALPACA_CREDS)
@@ -48,14 +49,11 @@ def update_params(params: strategyParams):
                         'bracket_sell_stop_loss_price': params.bracket_sell_stop_loss_multiplier,
                         'position_size': params.position_size})
     else:
-        strategy_instance.parameters = {'symbol': params.symbol,
-                                        'cash_at_risk': params.cash_at_risk, 
-                                        'bracket_buy_take_profit_price': params.bracket_buy_take_profit_multiplier, 
-                                        'bracket_buy_stop_loss_price': params.bracket_buy_stop_loss_multiplier, 
-                                        'bracket_sell_take_profit_price': params.bracket_sell_take_profit_multiplier,
-                                        'bracket_sell_stop_loss_price': params.bracket_sell_stop_loss_multiplier,
-                                        'position_size': params.position_size}
-        return {'message': 'Parameters updated'}
+        for var_name, value in vars(params).items():
+            if value is not None:
+                setattr(strategy_instance, var_name, value)
+    
+    return {'message': 'Parameters updated'}
 
 @app.post("/start")
 def execute_bot(params: strategyParams):
