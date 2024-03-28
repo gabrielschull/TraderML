b959be72e9ee048b46f1c36d704367bb457f4748
Gabriel <gschull991@gmail.com>
2024-03-28 17:06:13 -0400

    little fixes

--- frontend/src/components/ui/order-selector.tsx
+++ frontend/src/components/ui/order-selector.tsx
@@ -17,6 +17,7 @@ const limitSchema = z.object({
     sell_limit_multiplier: z.coerce.number().min(0),
     limit_order_expiry: z.string(),
     order_type: z.string(),
+    symbol: z.string(),
 }) 
 
 const bracketSchema = z.object({
@@ -27,6 +28,7 @@ const bracketSchema = z.object({
     bracket_buy_stop_loss_multiplier: z.coerce.number().min(0),
     bracket_sell_stop_loss_multiplier: z.coerce.number().min(0),
     order_type: z.string(),
+    symbol: z.string(),
 
 })
 
@@ -34,6 +36,7 @@ const marketSchema = z.object({
     sentiment_time_to_consider: z.coerce.number().min(0),
     sentiment_confidence_threshold: z.coerce.number().min(0).max(1),
     order_type: z.string(),
+    symbol: z.string(),
 
 })
 
@@ -50,6 +53,7 @@ export const OrderSelector = () => {
             buy_limit_multiplier: 1.01,
             sell_limit_multiplier: 0.99,
             limit_order_expiry: 'day',
+            symbol: 'SPY',
         }
     });
     const bracketForm = useForm<z.infer<typeof bracketSchema>>({ 
@@ -62,6 +66,7 @@ export const OrderSelector = () => {
             bracket_sell_take_profit_multiplier: 0.99,
             bracket_buy_stop_loss_multiplier: 0.98,
             bracket_sell_stop_loss_multiplier: 1.02,
+            symbol: 'SPY',
         }
     });
     const marketForm = useForm<z.infer<typeof marketSchema>>({
@@ -70,6 +75,7 @@ export const OrderSelector = () => {
             sentiment_time_to_consider: 3,
             sentiment_confidence_threshold: 0.999,
             order_type: 'market',
+            symbol: 'SPY',
         }
     });
 
@@ -228,6 +234,19 @@ export const OrderSelector = () => {
             )
         }}
         />
+        <FormField control={bracketForm.control} 
+        name='symbol'
+        render={({field}) => {
+            return ( <FormItem>
+                <FormLabel className="text-white">Stock Symbol To Buy And Compare</FormLabel>
+                <FormControl>
+                    <Input placeholder="Stock Symbol" {...field}/>
+                </FormControl>
+                <FormMessage/>
+            </FormItem>
+            )
+        }}
+        />
 
         </>
         )}
@@ -286,6 +305,19 @@ export const OrderSelector = () => {
             )
         }}
         />
+        <FormField control={limitForm.control} 
+        name='symbol'
+        render={({field}) => {
+            return ( <FormItem>
+                <FormLabel className="text-white">Stock Symbol To Buy And Compare</FormLabel>
+                <FormControl>
+                    <Input placeholder="Stock Symbol" {...field}/>
+                </FormControl>
+                <FormMessage/>
+            </FormItem>
+            )
+        }}
+        />
         </>
         )}
         {order_type== 'market'&& (
@@ -303,13 +335,26 @@ export const OrderSelector = () => {
             )
         }}
         />
-        <FormField control={limitForm.control} 
+        <FormField control={marketForm.control} 
         name='sentiment_confidence_threshold'
         render={({field}) => {
             return ( <FormItem>
                 <FormLabel className="text-white">Sentiment Confidence Threshold, 0 - 0.999</FormLabel>
                 <FormControl>
                     <Input placeholder="Sentiment Confidence Threshold" {...field}/>
+                </FormControl>
+                <FormMessage/>
+            </FormItem>
+            )
+        }}
+        />
+        <FormField control={marketForm.control} 
+        name='symbol'
+        render={({field}) => {
+            return ( <FormItem>
+                <FormLabel className="text-white">Stock Symbol To Buy And Compare</FormLabel>
+                <FormControl>
+                    <Input placeholder="Stock Symbol" {...field}/>
                 </FormControl>
                 <FormMessage/>
             </FormItem>
